9ddc8469bde5d5107773408cdfab403f6f73669c
anashmamouch <anashmamouch90@gmail.com>
2016-04-10 14:15:43 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,12 @@
+var express = require('express')
+var app = express();
+var PORT = process.env.port || 3000;
+
+//Simple get request
+app.get('/', function(req, res){
+  res.send('TODO api root');
+});
+
+app.listen(PORT, function(){
+  console.log('listening at port: '+ PORT + ' started...');
+});
