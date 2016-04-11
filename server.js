f75f2fc2d395eef2b4d232d64c54277173af5a7c
anashmamouch <anashmamouch90@gmail.com>
2016-04-11 13:27:08 +0100

    added underscore library

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyparser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -22,30 +23,31 @@ app.get('/todos', function(req, res){
 //GET todo by id
 app.get('/todos/:id', function(req, res){
   var todoId = parseInt(req.params.id, 10);
-  var matchedTodo;
-  todos.forEach(function(todo){
-    if(todo.id === todoId){
-      matchedTodo = todo;
-    }
-  });
+  var matchedTodo = _.findWhere(todos, {id: todoId});
 
   if (matchedTodo) {
     res.json(matchedTodo);
   }else{
-    res.status(404).send;
+    res.status(404).send();
   }
 
 });
 
 //POST todos
 app.post('/todos', function(req, res){
-  var body = req.body;
+  var body = _.pick(req.body, 'description', 'completed');
+
+  if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0 ) {
+      return res.status(400).send();
+  }
+
+  body.description = body.description.trim();
 
   //add id field
   body.id = todoNextId++;
 
   //push id into array
-  todos.push(body);  
+  todos.push(body);
 
   res.json(body);
 });
